4c8e2e5180158df9b8605152b448fef709f61525
George Bernard <ghb5@duke.edu>
2018-12-14 15:28:24 -0500

    added aliases and did file

---
+++ bash/.bash_aliases
@@ -0,0 +1,3 @@
+alias battery_left='cat /sys/class/power_supply/BAT0/capacity'
+
+alias did="vim +'normal Go' +'read!date' +'normal ^i# '  +'normal o' ~/did.md"
--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -2,6 +2,7 @@
 DOTFILES_DIR='~/dotfiles'
 
 ln -sfv $(realpath bash/.bashrc) ~
+ln -sfv $(realpath bash/.bash_aliases) ~
 ln -sfv $(realpath vim/.vimrc) ~
 ln -sfv $(realpath git/.gitconfig) ~
 ln -sfv $(realpath tmux/.tmux.conf) ~
